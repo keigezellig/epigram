dbc7d784d90ce698ce86f6b2d79deb6955efb7df
Maarten Joosten <mh.joosten666@gmail.com>
2024-07-31 22:12:10 +0200

    Initial project setup

---
+++ src/main/java/com/joco/epigram/EpigramApplication.java
@@ -0,0 +1,13 @@
+package com.joco.epigram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpigramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpigramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joco/epigram/EpigramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joco.epigram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpigramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
