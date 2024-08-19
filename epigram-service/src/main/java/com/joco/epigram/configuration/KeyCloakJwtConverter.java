eddad7657196e38d9dad739c9f7db22bb6f8f395
Maarten Joosten <mh.joosten666@gmail.com>
2024-08-19 22:02:24 +0200

    Simplified JWT converter by adding client roles on the top level of the token

--- epigram-service/src/main/java/com/joco/epigram/configuration/KeyCloakJwtConverter.java
+++ epigram-service/src/main/java/com/joco/epigram/configuration/KeyCloakJwtConverter.java
@@ -1,7 +1,6 @@
 package com.joco.epigram.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,36 +8,20 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 @Slf4j
 public class KeyCloakJwtConverter implements Converter<Jwt, JwtAuthenticationToken> {
 
-    private final ConfigProperties configProperties;
-
-    public KeyCloakJwtConverter(@Autowired ConfigProperties configProperties) {
-        this.configProperties = configProperties;
-    }
 
     @Override
     public JwtAuthenticationToken convert(Jwt jwt) {
-        String RESOURCE_ACCESS_KEY = "resource_access";
-        String REALM_ACCESS_KEY = "realm_access";
-        String ROLES_KEY = "roles";
+        String ROLES_KEY = "client_roles";
         String PRINCIPAL_ATTR = "preferred_username";
 
-        Stream<SimpleGrantedAuthority> resourcesAccess = Optional.of(jwt)
-                .map(token -> token.getClaimAsMap(RESOURCE_ACCESS_KEY))
-                .map(claimMap -> (Map<String, List<String>>) claimMap.get(configProperties.getKeycloakClientId()))
-                .map(resourceData -> resourceData.get(ROLES_KEY))
-                .stream()
-                .flatMap(Collection::stream)
-                .map(SimpleGrantedAuthority::new);
-
-        Set<GrantedAuthority> authorities = resourcesAccess.collect(Collectors.toSet());
+        List<GrantedAuthority> authorities = jwt.getClaimAsStringList(ROLES_KEY).stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
         String principalClaimName = jwt.getClaimAsString(PRINCIPAL_ATTR);
 
         return new JwtAuthenticationToken(jwt, authorities, principalClaimName);
